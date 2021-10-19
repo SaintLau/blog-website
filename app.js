9608b263424c404a1cb65aaf694c9e5383cc1234
Laura Santos <alsr16p@gmail.com>
2021-10-19 18:09:47 +0100

    compose working and rendering at home page

--- app.js
+++ app.js
@@ -13,10 +13,15 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+//to store posts:
+const posts = [];
 
 //set server && routes
 app.get('/', (req, res) => {
-    res.render('home', {startingContent: homeStartingContent});
+    res.render('home', {startingContent: homeStartingContent,
+    posts: posts
+});
+    
 });
 
 app.get('/about', (req, res) => {
@@ -32,8 +37,14 @@ app.get('/compose', (req, res) => {
 });
 
 app.post('/compose', (req, res) => {
-    //input name:
-    console.log(req.body.postTitle);
+    //create object to store the data from compose form:
+    const post = {
+        title: req.body.postTitle,
+        text: req.body.postBody
+    };
+    posts.push(post);
+
+    res.redirect('/');
 });
 
 app.listen(port, () => {
